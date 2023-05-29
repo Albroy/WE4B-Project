37eb635927d634189a096a1133f9e51a7ae5fc23
Rarynn <94757950+Rarynn@users.noreply.github.com>
2023-05-29 15:35:38 +0200

    addCards

--- VendMaRue/src/app/app.module.ts
+++ VendMaRue/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './cards/cards.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CardsComponent,
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
