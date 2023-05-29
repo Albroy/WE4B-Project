5c149dde043a56c66bd250d2ea7b21bb7b1ab1b7
Rarynn <94757950+Rarynn@users.noreply.github.com>
2023-05-29 16:51:12 +0200

    suppression h-100

--- VendMaRue/src/classes/Card.ts
+++ VendMaRue/src/classes/Card.ts
@@ -1,17 +1,15 @@
 export class Card {
 
-    constructor(
-      public photo: string,
-      public brand: string,
-      public price: string,
-      public text: string,
-      ) {
-  
-      this.photo = photo;
-      this.brand = brand;
-      this.price = price;
-      this.text = text;
+  constructor(
+    public photo: string,
+    public brand: string,
+    public price: string,
+    public text: string
+  ) {
+    this.photo = photo;
+    this.brand = brand;
+    this.price = price;
+    this.text = text;
 
-    }
   }
-  
+}
