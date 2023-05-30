77dd142bbad940290f31ea7029ae9249716628ae
Rarynn <94757950+Rarynn@users.noreply.github.com>
2023-05-30 16:51:03 +0200

    popup

--- VendMaRue/src/app/card.service.ts
+++ VendMaRue/src/app/card.service.ts
@@ -8,7 +8,7 @@ export class CardService {
   constructor() { }
 
   getCards()  {
-    this.Array.push(new Card(1,0,"../assets/test.jpg","UTBM",9.99,0,"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam quidem eaque ut eveniet aut quis rerum."));
+    this.Array.push(new Card(1,0,"../assets/test.jpg","UTBM",9.99,0,"Lorem, ipsum dolor sit amet conUUUUUUUUUUUUUUUUUUUUUUUUUUUUU UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUsectetur adipisicing elit. Veniam quidem eaque ut eveniet aut quis rerum."));
     this.Array.push(new Card(2,0,"../assets/test.jpg","UTBM",9.99,10,"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam quidem eaque ut eveniet aut quis rerum."));
     this.Array.push(new Card(3,0,"../assets/test.jpg","UTBM",50,50,"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam quidem eaque ut eveniet aut quis rerum."));
     this.Array.push(new Card(4,0,"../assets/test.jpg","UTBM",50,80,"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam quidem eaque ut eveniet aut quis rerum."));
--- VendMaRue/src/app/popup-card/popup-card.component.ts
+++ VendMaRue/src/app/popup-card/popup-card.component.ts
@@ -1,13 +1,21 @@
-import { Component,OnInit,Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Card } from '../../classes/Card';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-popup-card',
   templateUrl: './popup-card.component.html',
-  styleUrls: ['./popup-card.component.css']
+  styleUrls: ['./popup-card.component.css'!]
 })
 export class PopupCardComponent implements OnInit {
   @Input() card!: Card;
-  constructor() {};
-  ngOnInit(): void {};
+
+  constructor(private activeModal: NgbActiveModal) {}
+
+  ngOnInit(): void {}
+
+  closeModal(): void {
+    // Fermez le popup en utilisant la référence au modal
+    this.activeModal.close();
+  }
 }
