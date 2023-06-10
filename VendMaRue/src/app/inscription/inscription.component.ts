b513837fb04e9a42a250ab12c49f97eb8b0bbedc
Rarynn <94757950+Rarynn@users.noreply.github.com>
2023-06-10 17:05:03 +0200

    Debut profile

--- VendMaRue/src/app/inscription/inscription.component.ts
+++ VendMaRue/src/app/inscription/inscription.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { User } from 'src/classes/User';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
-export class InscriptionComponent {
+export class InscriptionComponent implements OnInit{
+  public user: User;
+
+  constructor(private userService: UserService, private router: Router) {
+    this.user = new User(this.userService.list_length + 1, '../assets/images/user.png', '', '', '', 0, new Date(), '', '')
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.user)
+  }
+
+  addUser() {
+    this.userService.addUser(this.user).subscribe(data => {
+      this.user = data
+    })
+    this.router.navigateByUrl('')
+  }
+  
 
 }
--- VendMaRue/src/app/user.service.ts
+++ VendMaRue/src/app/user.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {User} from '../classes/User';
-import {Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../classes/User';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  Array : User[] = [];
+  Array: User[] = [];
+  list_length!: number
 
-  constructor(private http : HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getData() : Observable<User[]> {
+  getData(): Observable<User[]> {
     return this.http.get<User[]>('http://localhost:3000/Users')
   }
+  getDataLength() {
+    this.http.get<User[]>('http://localhost:3000/Users').subscribe(data => {
+      this.list_length = (data).length
+    })
+  }
+  addUser(User : User): Observable<User>{
+    return this.http.post<User>('http://localhost:3000/Users', User)
+  }
+
 }
