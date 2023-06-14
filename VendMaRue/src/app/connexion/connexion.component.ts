5924ca8276bfa2c27023f99708cf1a50da8b6e59
Rarynn <94757950+Rarynn@users.noreply.github.com>
2023-06-14 19:52:53 +0200

    Correction connexion

--- VendMaRue/src/app/connexion/connexion.component.ts
+++ VendMaRue/src/app/connexion/connexion.component.ts
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
 export class ConnexionComponent implements OnInit {
   mail: string = "";
   mdp: string = "";
+  error: string = "";
   constructor(private userService: UserService, private router: Router) {
   }
 
@@ -17,9 +18,9 @@ export class ConnexionComponent implements OnInit {
   onSubmit() {
     if (this.mail && this.mdp) {
       this.mail = this.mail.toString();
-      this.userService.createUserSession(this.mail, this.mdp);  
-      this.router.navigateByUrl('');
-    }  //    
+      this.userService.createUserSession(this.mail, this.mdp);
+      sessionStorage.getItem('user') ? this.router.navigate(['']) : this.error = "Mot de passe ou email incorrect";
+    }
 
   }
 
